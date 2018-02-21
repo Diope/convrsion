64025e7ad60ace120872bc416c1b4afdb335ce04
Dion Pettaway <dionp@fullsail.edu>
2018-02-20 19:10:55 -0500

    Async reading of multiple videos metadata via promises

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const electron = require('electron');
-const {app, Browser} = electron;
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
+
+const {app, BrowserWindow, ipcMain} = electron;
+
+let mainWindow; // For garbage collection purposes
 
 app.on('ready', () => {
-  new BrowserWindow({
+  mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
     webPreferences: {backgroundThrottling: false}
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on('videos:added', (event, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        if (err) {
+          return Promise.reject(err)
+        }
+        resolve(metadata)
+      })
+    })
   })
-})
+  Promise.all(promises).then((metadata) => {console.log(metadata);})
+});
